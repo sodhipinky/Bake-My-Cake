458cb522052ca55240125c93269feaf1dca16145
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-06 16:48:59 +0530

    added HttpClientModule and FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
 import {ProductViewComponent} from './components/product-view/product-view.component';
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
 	declarations: [
@@ -22,6 +24,8 @@ import {ProductViewComponent} from './components/product-view/product-view.compo
 		BrowserModule,
 		AppRoutingModule,
 		BrowserAnimationsModule,
+		HttpClientModule,
+		FormsModule,
 		MatToolbarModule,
 		MatButtonModule,
 		MatSidenavModule,
