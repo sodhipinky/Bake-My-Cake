8720d5bfbea1e47cd80887aa0ce89dcdc7ab2b0f
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-06 16:44:31 +0530

    generated product-view component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatIconModule} from '@angular/material/icon';
 import {MatListModule} from '@angular/material/list';
+import {ProductViewComponent} from './components/product-view/product-view.component';
 
 @NgModule({
 	declarations: [
 		AppComponent,
-		HeaderComponent
+		HeaderComponent,
+		ProductViewComponent
 	],
 	imports: [
 		BrowserModule,
