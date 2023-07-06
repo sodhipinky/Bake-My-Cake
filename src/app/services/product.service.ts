bf7434cb9c203ad41d3c9da549a33569bb4a6072
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-06 16:52:25 +0530

    defined getAllProducts method

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,10 +1,18 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Product} from "../models/product.model";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class ProductService {
+	url: string = 'http://localhost:3000/products';
 
-	constructor() {
+	constructor(private httpClient: HttpClient) {
+	}
+
+	getAllProducts(): Observable<Array<Product>> {
+		return this.httpClient.get<Array<Product>>(url);
 	}
 }
