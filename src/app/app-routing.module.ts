064c269d6d90c1190e7638b2e459e93a9646c800
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-06 17:34:27 +0530

    defined route for all products

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {ProductViewComponent} from "./components/product-view/product-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		path: 'allProducts',
+		component: ProductViewComponent
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
