457d639210e5c34c5443916ed9df6eb4a2dd25fa
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-08 00:06:38 +0530

    defined EventEmitter

--- src/app/components/filter-product/filter-product.component.ts
+++ src/app/components/filter-product/filter-product.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
 	selector: 'app-filter-product',
@@ -6,5 +6,8 @@ import {Component} from '@angular/core';
 	styleUrls: ['./filter-product.component.css']
 })
 export class FilterProductComponent {
+	category: string = ''
 
+	@Output()
+	filterProductEvent: EventEmitter<string> = new EventEmitter<string>();
 }
