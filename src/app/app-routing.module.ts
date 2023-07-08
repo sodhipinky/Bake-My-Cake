ef808a57d77a2ecd798eb6ee58917eed6f5e76ae
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-08 15:35:09 +0530

    defined wild card route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ProductViewComponent} from "./components/product-view/product-view.component";
 import {ProductDetailComponent} from "./components/product-detail/product-detail.component";
+import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
 
 const routes: Routes = [
 	{
@@ -16,6 +17,10 @@ const routes: Routes = [
 		path: '',
 		redirectTo: 'products',
 		pathMatch: 'full'
+	},
+	{
+		path: '**',
+		component: PageNotFoundComponent
 	}
 ];
 
