ff9856a2b4c5d6832e0def316f70fdfc1f2894b2
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-08 13:08:42 +0530

    defined route for product detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ProductViewComponent} from "./components/product-view/product-view.component";
+import {ProductDetailComponent} from "./components/product-detail/product-detail.component";
 
 const routes: Routes = [
 	{
 		path: 'products',
 		component: ProductViewComponent
+	},
+	{
+		path: 'products/:id',
+		component: ProductDetailComponent
 	},
 	{
 		path: '',
