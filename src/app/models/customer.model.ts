65e808a19b59ee4c282329bcc99e5f083cda293d
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-10 12:59:43 +0530

    defined the customer class

--- src/app/models/customer.model.ts
+++ src/app/models/customer.model.ts
@@ -1,2 +1,15 @@
 export class Customer {
+	constructor(
+		public id: number,
+		public name: string,
+		public email: string,
+		public phone: string,
+		public address: {
+			street: string,
+			city: string,
+			state: string,
+			pinCode: number
+		}
+	) {
+	}
 }
