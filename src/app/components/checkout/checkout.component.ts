0c375310f3bfdca9623f252fb0ed39bdc749323c
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-10 13:52:05 +0530

    defined properties for checkout

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Product} from "../../models/product.model";
 
 @Component({
 	selector: 'app-checkout',
@@ -6,5 +7,6 @@ import {Component} from '@angular/core';
 	styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent {
-
+	product: Product | undefined;
+	quantity: number = 0;
 }
