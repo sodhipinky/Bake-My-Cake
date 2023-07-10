4d55ab1ade1ab760dc86a4cde3ca600dc00491d6
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-10 16:36:23 +0530

    defined saveCustomer method

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,10 +1,19 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Customer} from "../models/customer.model";
+import {Observable} from "rxjs";
 
 @Injectable({
 	providedIn: 'root'
 })
 export class CustomerService {
 
-	constructor() {
+	url: string = 'http://localhost:3001/customers'
+
+	constructor(private httpClient: HttpClient) {
+	}
+
+	saveCustomer(customer: Customer): Observable<Customer> {
+		return this.httpClient.post<Customer>(this.url, customer);
 	}
 }
