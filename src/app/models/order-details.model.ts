ca02bf63d80a48dc87f8521ebf1ac90724e9c6a1
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-10 23:18:51 +0530

    defined orderDetails type

--- src/app/models/order-details.model.ts
+++ src/app/models/order-details.model.ts
@@ -1,2 +1,19 @@
 export class OrderDetails {
+	constructor(
+		public orderId: number,
+		public productName: string,
+		public quantity: number,
+		public price: number,
+		public amount: number,
+		public customerName: string,
+		public customerEmail: string,
+		public customerPhone: string,
+		public customerAddress: {
+			customerStreet: string,
+			customerCity: string,
+			customerState: string,
+			customerPinCode: number
+		}
+	) {
+	}
 }
