7033a0fda2e56e8aa96b715294a70f97d7c2c0ab
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-11 01:16:43 +0530

    injected authentication and router service

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,8 @@ import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 import {Observable} from 'rxjs';
 import {map, shareReplay} from 'rxjs/operators';
 import {LoginComponent} from "../login/login.component";
+import {AuthenticationService} from "../../services/authentication.service";
+import {Router} from "@angular/router";
 
 @Component({
 	selector: 'app-header',
@@ -12,13 +14,16 @@ import {LoginComponent} from "../login/login.component";
 export class HeaderComponent {
 	@Input()
 	loggedIn: boolean = false;
-
 	private breakpointObserver = inject(BreakpointObserver);
 	isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
 		.pipe(
 			map(result => result.matches),
 			shareReplay()
 		);
+
+	constructor(private authenticationService: AuthenticationService,
+				private router: Router) {
+	}
 
 	loginEvent($event: any) {
 		this.loggedIn = !($event instanceof LoginComponent)
