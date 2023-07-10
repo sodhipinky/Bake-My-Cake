c6ba228b819ffb2062aaf23722323d50f812edf0
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-10 13:01:08 +0530

    defined the order class

--- src/app/models/order.model.ts
+++ src/app/models/order.model.ts
@@ -1,2 +1,11 @@
 export class Order {
+	constructor(
+		public id: number,
+		public productId: number,
+		public customerId: number,
+		public date: Date,
+		public quantity: number,
+		public amount: number
+	) {
+	}
 }
