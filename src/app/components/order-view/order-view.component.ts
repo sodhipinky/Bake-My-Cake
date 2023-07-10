adb2a2f349aaf1ffa400f34f4b4aab3534f421b9
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-10 23:14:25 +0530

    generated orderDetails model

--- src/app/components/order-view/order-view.component.ts
+++ src/app/components/order-view/order-view.component.ts
@@ -11,7 +11,6 @@ import {ProductService} from "../../services/product.service";
 })
 export class OrderViewComponent implements OnInit {
 	orders: Order[] = [];
-	orderDetails: [] = [];
 
 	constructor(private orderService: OrderService,
 				private customerService: CustomerService,
@@ -19,33 +18,5 @@ export class OrderViewComponent implements OnInit {
 	}
 
 	ngOnInit(): void {
-		this.orderService.getOrders().subscribe(orders => {
-			this.orders = orders;
-		});
-		this.orders.forEach(order => {
-			let orderDetail: {};
-			this.productService.getProductById(order.productId).subscribe(product => {
-				orderDetail = {
-					'orderId': order.id,
-					'orderDate': order.date,
-					'productName': product.name,
-					'quantity': order.quantity,
-					'price': product.price
-				}
-			});
-			this.customerService.getCustomerById(order.customerId).subscribe(customer => {
-				orderDetail = {
-					...orderDetail,
-					'customerName': customer.name,
-					'customerEmail': customer.email,
-					'customerPhone': customer.phone,
-					'customerStreet': customer.address.street,
-					'customerCity': customer.address.city,
-					'customerState': customer.address.state,
-					'customerPinCode': customer.address.pinCode,
-					'amount': order.amount
-				}
-			});
-		})
 	}
 }
