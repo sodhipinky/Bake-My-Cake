8ff18cf7bd2eb66607bee93af4f5735be3824894
Pinky Sodhi <pinky.sodhi@niit.com>
2023-07-11 01:52:09 +0530

    defined canDeactivate method

--- src/app/services/prevent.guard.ts
+++ src/app/services/prevent.guard.ts
@@ -1,5 +1,6 @@
 import {CanDeactivateFn} from '@angular/router';
+import {CheckoutComponent} from "../components/checkout/checkout.component";
 
-export const preventGuard: CanDeactivateFn<unknown> = (component, currentRoute, currentState, nextState) => {
-	return true;
+export const preventGuard: CanDeactivateFn<CheckoutComponent> = (component, currentRoute, currentState, nextState) => {
+	return component.isSubmitted();
 };
